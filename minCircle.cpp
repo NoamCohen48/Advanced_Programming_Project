4baf063cc58f54d20671509d26e53621f9006b7d
shakedc1599 <47411973+shakedc1599@users.noreply.github.com>
2021-12-01 10:22:02 +0200

    circleFrom3Points circleFrom2Points

--- minCircle.cpp
+++ minCircle.cpp
@@ -86,3 +86,30 @@ Circle findMinCircle(Point **points, size_t size) {
     boundary.reserve(3);
     recursiveFindMinCircle(points, boundary, size);
 }
+
+// Function to return a unique circle that
+// intersects three points
+Circle circleFrom3Points(const Point& A, const Point& B, const Point& C)
+{
+    double newB = (B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y);
+    double newC = (C.x - A.x) * (C.x - A.x) + (C.y - A.y) * (C.y - A.y);
+    double newD = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
+
+    Point *center = new Point(((((C.y - A.y) * newB - (B.y - A.y) * newC) / (2 * newD))),
+                              (((B.x - A.x) * newC - (C.x - A.x) * newB) / (2 * newD)));
+    center->x += A.x;
+    center->y += A.y;
+
+    return Circle(*center, center->dist(A));
+}
+
+// Function to return the smallest circle
+// that intersects 2 points
+Circle circleFrom2Points(const Point &p1, const Point &p2)
+{
+    // Set the center to be the midpoint of A and p2
+    Point *center = new Point(((p1.x + p2.x) / 2.0), ((p1.y + p2.y) / 2.0));
+
+    // Set the radius to be half the distance between p1 and p2
+    return Circle(*center, (p1.dist(p2) / 2.0));
+}
