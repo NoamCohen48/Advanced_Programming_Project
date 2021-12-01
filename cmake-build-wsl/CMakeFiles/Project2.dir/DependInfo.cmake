8c74c0bd88ddb816928a90cb1168ce5bb8f8e88a
Noam <cohennoam48@gmail.com>
2021-12-01 10:23:19 +0200

    added functions to circle

---
+++ cmake-build-wsl/CMakeFiles/Project2.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/MainTrain.cpp" "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/cmake-build-wsl/CMakeFiles/Project2.dir/MainTrain.cpp.o"
+  "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/SimpleAnomalyDetector.cpp" "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/cmake-build-wsl/CMakeFiles/Project2.dir/SimpleAnomalyDetector.cpp.o"
+  "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/anomaly_detection_util.cpp" "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/cmake-build-wsl/CMakeFiles/Project2.dir/anomaly_detection_util.cpp.o"
+  "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/minCircle.cpp" "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/cmake-build-wsl/CMakeFiles/Project2.dir/minCircle.cpp.o"
+  "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/timeseries.cpp" "/mnt/d/OneDrive - Bar-Ilan University/BIU - Advanced Programing/Project2/cmake-build-wsl/CMakeFiles/Project2.dir/timeseries.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
