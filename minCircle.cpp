42a097bd7e79ae5b6308029f2b20bee9b7e4ee90
Noam <cohennoam48@gmail.com>
2021-12-01 10:29:29 +0200

    fixed all! working now trust me im a genius

--- minCircle.cpp
+++ minCircle.cpp
@@ -5,10 +5,6 @@ bool isInside(const Point &p, const Circle &d) {
     return p.dist(d.center) <= d.radius;
 }
 
-Circle circleFrom2Points(Point *p1, Point *p2) {
-
-}
-
 bool isValid(Circle circle, vector<Point *> boundary) {
     // going throughout all points to check if inside circle
     for (Point *point: boundary) {
@@ -19,8 +15,30 @@ bool isValid(Circle circle, vector<Point *> boundary) {
     return true;
 }
 
-Circle circleFrom2Points(Point *p1, Point *p2, Point *p3) {
+// Function to return a unique circle that
+// intersects three points
+Circle circleFrom3Points(const Point &A, const Point &B, const Point &C) {
+    double newB = (B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y);
+    double newC = (C.x - A.x) * (C.x - A.x) + (C.y - A.y) * (C.y - A.y);
+    double newD = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
 
+    Point *center = new Point(((((C.y - A.y) * newB - (B.y - A.y) * newC) / (2 * newD))),
+                              (((B.x - A.x) * newC - (C.x - A.x) * newB) / (2 * newD)));
+
+    center->x += A.x;
+    center->y += A.y;
+
+    return Circle(*center, center->dist(A));
+}
+
+// Function to return the smallest circle
+// that intersects 2 points
+Circle circleFrom2Points(const Point &p1, const Point &p2) {
+    // Set the center to be the midpoint of A and p2
+    Point *center = new Point(((p1.x + p2.x) / 2.0), ((p1.y + p2.y) / 2.0));
+
+    // Set the radius to be half the distance between p1 and p2
+    return Circle(*center, (p1.dist(p2) / 2.0));
 }
 
 Circle baseMinCircle(vector<Point *> boundary) {
@@ -36,7 +54,7 @@ Circle baseMinCircle(vector<Point *> boundary) {
 
     // if boundary is 2
     if (boundary.size() == 2) {
-        return circleFrom2Points(boundary[0], boundary[1]);
+        return circleFrom2Points(*boundary[0], *boundary[1]);
     }
 
     // if boundary is 3 - try to make circle from pair
@@ -45,7 +63,7 @@ Circle baseMinCircle(vector<Point *> boundary) {
         for (int second = first + 1; second < boundary.size(); ++second) {
 
             // calculating circle from pair
-            Circle circle = circleFrom2Points(boundary[first], boundary[second]);
+            Circle circle = circleFrom2Points(*boundary[first], *boundary[second]);
 
             // if the circle is valid return it
             if (isValid(circle, boundary)) {
@@ -54,8 +72,8 @@ Circle baseMinCircle(vector<Point *> boundary) {
         }
     }
 
-    // calculating cicle from 3 points
-    return circleFrom2Points(boundary[0], boundary[1], boundary[2]);
+    // calculating circle from 3 points
+    return circleFrom3Points(*boundary[0], *boundary[1], *boundary[2]);
 }
 
 
@@ -87,29 +105,3 @@ Circle findMinCircle(Point **points, size_t size) {
     recursiveFindMinCircle(points, boundary, size);
 }
 
-// Function to return a unique circle that
-// intersects three points
-Circle circleFrom3Points(const Point& A, const Point& B, const Point& C)
-{
-    double newB = (B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y);
-    double newC = (C.x - A.x) * (C.x - A.x) + (C.y - A.y) * (C.y - A.y);
-    double newD = (B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x);
-
-    Point *center = new Point(((((C.y - A.y) * newB - (B.y - A.y) * newC) / (2 * newD))),
-                              (((B.x - A.x) * newC - (C.x - A.x) * newB) / (2 * newD)));
-    center->x += A.x;
-    center->y += A.y;
-
-    return Circle(*center, center->dist(A));
-}
-
-// Function to return the smallest circle
-// that intersects 2 points
-Circle circleFrom2Points(const Point &p1, const Point &p2)
-{
-    // Set the center to be the midpoint of A and p2
-    Point *center = new Point(((p1.x + p2.x) / 2.0), ((p1.y + p2.y) / 2.0));
-
-    // Set the radius to be half the distance between p1 and p2
-    return Circle(*center, (p1.dist(p2) / 2.0));
-}
