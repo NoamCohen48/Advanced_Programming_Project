3c4cee596906911304ee8628658c491d865f71f4
noam <cohennoam48@gmail.com>
2021-12-12 13:29:30 +0200

    almost finished hs a bug

--- commands.h
+++ commands.h
@@ -12,17 +12,26 @@
 
 using namespace std;
 
-class DefaultIO{
+class DefaultIO {
 public:
-    virtual string read()=0;
-    virtual void write(string text)=0;
-    virtual void write(float f)=0;
-    virtual void read(float* f)=0;
-    virtual ~DefaultIO(){}
+    virtual string read() = 0;
+
+    virtual void write(string text) = 0;
+
+    virtual void write(float f) = 0;
+
+    virtual void read(float *f) = 0;
+
+    virtual ~DefaultIO() {}
 
     // you may add additional methods here
-    void readFile(string filePath){
-
+    void readFile(const string &filePath) {
+        ofstream file(filePath);
+        string line = read();
+        while (line != "done") {
+            file << line;
+        }
+        file.close();
     }
 };
 
@@ -30,29 +39,42 @@ public:
 
 
 // you may edit this class
-class Command{
+class Command {
 protected:
-    DefaultIO* dio;
+    DefaultIO *dio;
 public:
-    Command(DefaultIO* dio):dio(dio){}
-    virtual ~Command(){}
-    virtual void execute()=0;
+    Command(DefaultIO *dio) : dio(dio) {}
+
+    virtual ~Command() {}
+
+    virtual void execute() = 0;
+
     string description;
 };
 
 // implement here your command classes
+
+// option 1
 class uploadCSVCommand : public Command {
 public:
     string description = "upload a time series csv file\n";
+
     void execute() override {
+        // get train csv
         dio->write("please upload your local train csv file.\n");
+        dio->readFile("anomalyTrain.csv");
+        dio->write("Upload complete.\n");
 
-
+        // get test csv
+        dio->write("please upload your local test csv file.\n");
+        dio->readFile("anomalyTest.csv");
+        dio->write("Upload complete.\n");
     }
 };
 
-class algorithmSettingsCommand : public Command {
-    string description = "algorithm settings\n";
+class detectAnomaliesCommand : public Command {
+    string description = "detect anomalies\n";
+
     void execute() override {
 
         float wantedThreshold;
@@ -80,5 +102,33 @@ class algorithmSettingsCommand : public Command {
 
 };
 
+class algorithmSettingsCommand : public Command {
+    string description = "algorithm settings\n";
+
+    void execute() override {
+
+        float wantedThreshold;
+
+        // Get the pearson threshold.
+        double pearsonThreshold = (new HybridAnomalyDetector())->getPearsonThreshold();
+
+        while (true) {
+
+            // Write to the client.
+            dio->write("The current correlation threshold is " + to_string(pearsonThreshold) + "\n");
+
+            // Get wanted threshold from the client.
+            dio->read(&wantedThreshold);
+
+            // Check if the Threshold is between 0 and 1.
+            if (wantedThreshold >= 0 && wantedThreshold <= 1) {
+
+                // TODO save new Threshold.
+                break;
+            }
+            dio->write("please choose a value between 0 and 1.\n");
+        }
+    }
+};
 
 #endif /* COMMANDS_H_ */
